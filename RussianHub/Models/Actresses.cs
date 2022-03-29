e0bbbd768a4150b518406f88e67e42e8f09f0a1a
BioMihanoid <pechnikovmichelle@gmail.com>
2022-03-30 00:04:23 +0300

    i rot ebal etogo proecta

---
+++ RussianHub/Models/Actresses.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RussianHub.Models
+{
+    public class Actresses
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+    }
+}
