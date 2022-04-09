12cd87e60eda9ffd6c41c91ad1e2818ac606b6bb
BioMihanoid <pechnikovmichelle@gmail.com>
2022-04-10 00:33:34 +0300

    save commit

---
+++ RussianHub/Models/Entity/Photo.cs
@@ -0,0 +1,9 @@
+﻿namespace RussianHub.Models.Entity
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+        public String? Url { get; set; }
+        public String? AltText { get; set; }
+    }
+}
--- RussianHub/Program.cs
+++ RussianHub/Program.cs
@@ -1,57 +1,27 @@
-using Microsoft.EntityFrameworkCore;
-using NLog;
-using RussianHub.Models.Context;
+var builder = WebApplication.CreateBuilder(args);
 
-namespace RussianHub
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var config = new NLog.Config.LoggingConfiguration();
-
-            // Targets where to log to: File and Console
-            var logfile = new NLog.Targets.FileTarget("logfile") { FileName = "file.txt" };
-            var logconsole = new NLog.Targets.ConsoleTarget("logconsole");
-
-            // Rules for mapping loggers to targets            
-            config.AddRule(NLog.LogLevel.Info, NLog.LogLevel.Fatal, logconsole);
-            config.AddRule(NLog.LogLevel.Debug, NLog.LogLevel.Fatal, logfile);
-
-            // Apply config           
-            NLog.LogManager.Configuration = config;
-
-            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
-            builder.Services.AddDbContext<UserContext>(option => option.UseSqlServer(
-                builder.Configuration.GetConnectionString("DefaultConnection") ?? string.Empty
-                ));
-
-            WebApplication app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-
-            app.Run();
-        }
-
-    }
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
