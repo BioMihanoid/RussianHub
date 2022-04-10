560245c05376fb1290862c392c8ae78c94ddad61
BioMihanoid <pechnikovmichelle@gmail.com>
2022-04-10 22:06:09 +0300

    4 database (50 % working)

---
+++ RussianHub/Data/CommentContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RussianHub.Models;
+
+namespace RussianHub.Data
+{
+    public class CommentContext : DbContext
+    {
+        public CommentContext (DbContextOptions<CommentContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RussianHub.Models.Comment> Comment { get; set; }
+    }
+}
--- RussianHub/Models/Entity/PersonalParameters.cs
+++ RussianHub/Models/Entity/PersonalParameters.cs
@@ -1,19 +1,33 @@
-﻿namespace RussianHub.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RussianHub.Models;
 
 public class PersonalParameters
 {
     public int Id { get; set; }
+    [Display(Name = "Возраст")]
     public int Ade { get; set; }
+    [Display(Name = "Вес")]
     public double Weigh { get; set; }
+    [Display(Name = "Рост")]
     public double Height { get; set; }
+    [Display(Name = "Ник")]
     public string Nickname { get; set; } = null!;
+    [Display(Name = "Имя")]
     public string RealName { get; set; } = null!;
+    [Display(Name = "Город")]
     public string City { get; set; } = null!;
+    [Display(Name = "Пол")]
     public string Gender { get; set; } = null!;
+    [Display(Name = "Информация о себе")]
     public string Info { get; set; } = null!;
+    [Display(Name = "Цвет глаз")]
     public string EyeColor { get; set; } = null!;
+    [Display(Name = "Цвет волос")]
     public string HairColor { get; set; } = null!;
+    [Display(Name = "Характер")]
     public string Character { get; set; } = null!;
-    public string Photo { get; set; } = null!; //что-то придумать с фотографиями
+    //что-то придумать с фотографиями
+    [Display(Name = "Дата рождения")]
     public DateTime DatefBirth { get; set; }
 }
