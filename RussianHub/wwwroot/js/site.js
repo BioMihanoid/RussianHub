5c8ece68df5a149596969c77b82c321f867df5d8
gh0stman447 <o_malygin@bk.ru>
2022-04-30 15:40:56 +0300

    Video markup v.2

--- RussianHub/wwwroot/js/site.js
+++ RussianHub/wwwroot/js/site.js
@@ -2,3 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+
+/*VIDEO JS*/
+
+$(function () {
+    var $refreshButton = $('#refresh');
+    var $results = $('#css_result');
+
+    function refresh() {
+        var css = $('style.cp-pen-styles').text();
+        $results.html(css);
+    }
+
+    refresh();
+    $refreshButton.click(refresh);
+
+    // Select all the contents when clicked
+    $results.click(function () {
+        $(this).select();
+    });
+});
