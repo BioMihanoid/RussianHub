07d3ea4285f7f37f0f98d6a193ef2aeea64155b0
BioMihanoid <pechnikovmichelle@gmail.com>
2022-05-09 22:07:24 +0300

    add realated video

---
+++ RussianHub/Views/VeiwComponents/RealatedVideoViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RussianHub.Data;
+
+namespace RussianHub.Views.VeiwComponents
+{
+    public class RealatedVideoViewComponent : ViewComponent
+    {
+        private readonly VideoContext _videoDataBase;
+
+        public RealatedVideoViewComponent(VideoContext videoDataBase)
+        {
+            _videoDataBase = videoDataBase;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            //TODO add logic choose video
+            return View(_videoDataBase.Video.ToList());
+        }
+    }
+}
